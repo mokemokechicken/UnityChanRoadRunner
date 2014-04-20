7a572d5cd6db8e3ce8ac6cfeee4e5e1455c779fb
Ken Morishita <k_morishita@yumemi.co.jp>
2014-04-20 19:03:47 +0900

    modify BlockControl.cs

--- Assets/Script/BlockControl.cs
+++ Assets/Script/BlockControl.cs
@@ -2,26 +2,18 @@
 using System.Collections;
 
 public class BlockControl : MonoBehaviour {
-	private const int DISAPPEAR_TIMER_COUNT = 60*3;
-	private int disappearTimer = 0;
-
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (disappearTimer > 0) {
-			disappearTimer--;
-		}
 	}
 
 	void HitGun() {
-		if (disappearTimer > 0) {
+		if (animation.IsPlaying("CubeBreaking")) {
 			return;
 		}
-		disappearTimer = DISAPPEAR_TIMER_COUNT;
 		animation.Play("CubeBreaking");
 	}
-
 }
