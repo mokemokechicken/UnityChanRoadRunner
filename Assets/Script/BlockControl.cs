66547126ad56c54217805e40f6aba770ed5d1845
Ken Morishita <k_morishita@yumemi.co.jp>
2014-04-20 12:15:30 +0900

    This Version: http://qiita.com/mokemokechicken/items/7d9abe11e99d2235092b

---
+++ Assets/Script/BlockControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockControl : MonoBehaviour {
+	private const int DISAPPEAR_TIMER_COUNT = 60*3;
+	private int disappearTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (disappearTimer > 0) {
+			disappearTimer--;
+		}
+		renderer.enabled = collider.enabled = disappearTimer == 0;
+	}
+
+	void HitGun() {
+		if (disappearTimer > 0) {
+			return;
+		}
+		disappearTimer = DISAPPEAR_TIMER_COUNT;
+	}
+}
--- Assets/Script/UnityChanDemo1.cs
+++ Assets/Script/UnityChanDemo1.cs
@@ -5,8 +5,10 @@ public class UnityChanDemo1 : MonoBehaviour {
 	private const string LADDER = "Ladder";
 	private const string BAR = "Bar";
 	private const string BLOCK = "Block";
+	private const string BEAM = "Beam";
 
 	private Animator animator;
+	public GameObject beamPrefab;
 
 	public int ladderEnter = 0;
 	public int barEnter = 0;
@@ -21,11 +23,11 @@ public class UnityChanDemo1 : MonoBehaviour {
 	void Update () {
 		bool is_running = false;
 		if (!isFalling) { // If Not Falling, can move to Left/Right
-			if (Input.GetKey("right")) {
+			if (Input.GetKey(KeyCode.RightArrow)) {
 				transform.position += new Vector3(0.05f, 0, 0);
 				transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
 				is_running = true;
-			} else if (Input.GetKey ("left")) {
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
 				transform.position += new Vector3(-0.05f, 0, 0);
 				transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
 				is_running = true;
@@ -34,11 +36,11 @@ public class UnityChanDemo1 : MonoBehaviour {
 		animator.SetBool("is_running", is_running);
 
 		// if entering in any ladder collision, can move Up/Down
-		if (Input.GetKey("up") && ladderEnter > 0) {
+		if (Input.GetKey(KeyCode.UpArrow) && ladderEnter > 0) {
 				transform.position += new Vector3(0, 0.05f, 0);
-		} else if (Input.GetKey("down") && ladderEnter > 0) {
+		} else if (Input.GetKey(KeyCode.DownArrow) && ladderEnter > 0) {
 			transform.position += new Vector3(0, -0.05f, 0);
-		} else if (Input.GetKey("down") && barEnter > 0) { // if entering in bar and KeyDown, bar are released.
+		} else if (Input.GetKey(KeyCode.DownArrow) && barEnter > 0) { // if entering in bar and KeyDown, bar are released.
 			barEnter = 0;
 		}
 
@@ -50,12 +52,34 @@ public class UnityChanDemo1 : MonoBehaviour {
 			rigidbody.useGravity = true;
 		}
 
+		// Gun
+		int gun = Input.GetKey(KeyCode.Z) ? -1 : Input.GetKey(KeyCode.X) ? 1 : 0;
+		if (gun != 0 && !isFalling) {
+			RaycastHit hit;
+			Vector3 fromPos = transform.position+Vector3.up;
+			Vector3 direction = new Vector3(gun, -1, 0);
+			float length = 1.8f;
+			Debug.DrawRay(fromPos, direction.normalized * length, Color.green, 1, false);
+			if (Physics.Raycast(fromPos, direction, out hit, length)) {
+				if (canDestroyBlock(hit.transform)) {
+					hit.transform.gameObject.SendMessage("HitGun");
+				}
+ 			}
+		}
+
 		// if Velocity.y reach some degree, Falling is True
 		if (rigidbody.velocity.y <  -0.05f) {
 			isFalling = true;
 		} else {
 			isFalling = false;
 		}
+	}
+
+	private bool canDestroyBlock(Transform block) {
+		return 
+			block.tag == BLOCK && 
+			Mathf.Abs(block.position.x - transform.position.x) < 1.2f &&
+			transform.position.y - block.position.y > 0.3;
 	}
 
 	void OnTriggerEnter(Collider other) {
